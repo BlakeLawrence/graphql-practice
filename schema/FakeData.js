4d4ed8ae67c442d7afbd404176045a48658b2aff
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-02-23 11:29:40 +0000

    added some dummy data in a file : FakeData.js

---
+++ schema/FakeData.js
@@ -0,0 +1,26 @@
+export const userList = [
+  {
+    name: "Blake",
+    username: "blake",
+    age: 29,
+    nationality: "South African",
+  },
+  {
+    name: "Helen",
+    username: "Macks",
+    age: 39,
+    nationality: "South African",
+  },
+  {
+    name: "Justin",
+    username: "juzzy",
+    age: 27,
+    nationality: "American",
+  },
+  {
+    name: "Ed",
+    username: "eddy123",
+    age: 40,
+    nationality: "Australian",
+  },
+];
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,7 +1,14 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-type Query {
-    
-}
+  type User {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
 `;
