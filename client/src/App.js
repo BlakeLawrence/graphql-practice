8e1432eec40702b369fc37a0136f251df994c0cf
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-03-06 10:19:02 +0000

    added some comments about the apollo provider and client object for future reference

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import DisplayData from "./DisplayData";
 
 function App() {
   const client = new ApolloClient({
+    //inMemoryCache allows graphql to cache data into your browser to avoid refreshing pages etc.
     cache: new InMemoryCache(),
     uri: "http://localhost:4000/graphql",
   });
 
   return (
+    // we wrap all components that should be able to make requests on gql with the apollo provider. We pass the client variable that we created above as the prop. We have now established a connection with our Gql API.
     <ApolloProvider client={client}>
       <div className="App">
         <DisplayData />
