679aa4f8d0c7e035f9915f212627232225d9fdef
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-03-06 11:14:56 +0000

    added more comments for future refs and cleaned up console.logs. now working on first mutations

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { useQuery, useLazyQuery, gql } from "@apollo/client";
+// Import comments:
+// useQuery allows us to make queries to our GQL API - it works by fetching the data whenever our component renders
+
+// useLazyQuery...
+
+// gql is for gql statement, it is just for when we write a qgl statement (gql ` `) - like we learnt in the sandbox and how we wrote statements in the typeDefs (example of it in use below for QUERY_ALL_USERS)
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -23,7 +29,7 @@ const QUERY_ALL_MOVIES = gql`
   }
 `;
 
-const getMovieByName = gql`
+const GET_MOVIE_BY_NAME = gql`
   query getMovieByName($name: String!) {
     movie(name: $name) {
       name
@@ -37,37 +43,32 @@ function DisplayData() {
   const { data, loading, error } = useQuery(QUERY_ALL_USERS);
   const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
   const [fetchMovie, { data: movieSearchedData, error: movieError }] =
-    useLazyQuery(getMovieByName);
+    useLazyQuery(GET_MOVIE_BY_NAME);
 
   if (loading) {
     // use laoding spinner
-    return <h1>Data is Loading</h1>;
-  }
-
-  if (data) {
-    console.log(data);
-  }
-  if (movieData) {
-    console.log(movieData);
+    return <h1>Data is Loading...</h1>;
   }
 
   if (error) {
     console.log(error);
   }
-  if (movieError) {
-    console.log(movieError);
-  }
 
   return (
     <div>
+      {/* creating a user using mutations  */}
+      <div>
+        <button>Create User</button>
+      </div>
+      {/* mapping over users to display user data */}
       {data &&
         data.users.map((user) => {
           return (
             <div>
-              <div>Name: {user.name}</div>
-              <div>Username: {user.username}</div>
-              <div>Age: {user.age}</div>
-              <div>Natioanlity: {user.nationality}</div>
+              <h1>Name: {user.name}</h1>
+              <h2>Username: {user.username}</h2>
+              <h3>Age: {user.age}</h3>
+              <h4>Nationality: {user.nationality}</h4>
               <br />
             </div>
           );
@@ -76,14 +77,12 @@ function DisplayData() {
         movieData.movies.map((movie) => {
           return (
             <div>
-              <div>Name: {movie.name}</div>
-              <div>Release Date: {movie.yearOfPublication}</div>
-
+              <h3>Name: {movie.name}</h3>
+              <h4>Release Date: {movie.yearOfPublication}</h4>
               <br />
             </div>
           );
         })}
-
       <div>
         <input
           value={movieSearched}
